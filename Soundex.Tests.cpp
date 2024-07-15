16739758aa9e0756b60d0383e1f17b750f798cc7
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-15 10:35:22 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,41 +1,41 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
- 
-//Append 3 zeros if result contains less than 3 digits
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
-//AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
-  ASSERT_STREQ(soundex,"A200");
+
+// Append 3 zeros if result contains less than 3 digits
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+    // AAA
+    char soundex[5];
+    generateSoundex("BX", soundex);
+    ASSERT_STREQ(soundex, "B200");
 }
- 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
-//AAA
-  char soundex[5];
-  generateSoundex("A1CDFe", soundex);
-  ASSERT_STREQ(soundex,"A231");
+
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+    // AAA
+    char soundex[5];
+    generateSoundex("B2DFGh", soundex);
+    ASSERT_STREQ(soundex, "B231");
 }
- 
-//Test case to ckeck special character
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
-//AAA
-  char soundex[5];
-  generateSoundex("A[", soundex);
-  ASSERT_STREQ(soundex,"A000");
+
+// Test case to check special character
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+    // AAA
+    char soundex[5];
+    generateSoundex("B#", soundex);
+    ASSERT_STREQ(soundex, "B000");
 }
- 
-//Replace all adjacent same digits with one digit
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
-//AAA
-  char soundex[5];
-  generateSoundex("Abfp", soundex);
-  ASSERT_STREQ(soundex,"A100");
+
+// Replace all adjacent same digits with one digit
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
+    // AAA
+    char soundex[5];
+    generateSoundex("Bgfp", soundex);
+    ASSERT_STREQ(soundex, "B100");
 }
- 
-//two letters with the same number separated by 'h', 'w' or 'y' are coded as a single number
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
-//AAA
-  char soundex[5];
-  generateSoundex("BShZ", soundex);
-  ASSERT_STREQ(soundex,"B200");
+
+// Two letters with the same number separated by 'h', 'w' or 'y' are coded as a single number
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
+    // AAA
+    char soundex[5];
+    generateSoundex("DSjZ", soundex);
+    ASSERT_STREQ(soundex, "D200");
 }
